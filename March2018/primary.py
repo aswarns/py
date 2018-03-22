74eb7a9e71a88092befd6ba4dc585bfff3f90b84
ashish_swarn_singh <ashish.swarn.singh@oracle.com>
2018-03-22 10:31:30 +0530

    my first calc program primary.py

--- March2018/primary.py
+++ March2018/primary.py
@@ -1,64 +1,49 @@
 import time
 
-def payments(bill,ppl):
-  try:
-    return round((bill/ppl),2)
-  except:
-    print("An error in payment calculations")
-
-
-def tip(bill,perc,ppl):
-  try:
-    return round(((bill * (perc/100.00))/ppl),2)
-  except:
-    print("An error while calculation TIP")
-
-
-
-
 def main():
-  
-  print(" How much is the Bill ? ")
+
+
   while True:
     try:
-      total_bill = float(input(">> "))
+      print('Whats the bill amount')
+      bill = float(input(">> Rs.."))
       break
     except:
-      print("\n You must enter a number value \n")
+      print("\n Pls enter number value only\n")
 
 
-  print("How many people ?")
   while True:
     try:
-      num_ppl = int(input(">>"))
+      print('Whats the number of people')
+      people = int(input(">> people.."))
       break
     except:
-      print("\n Must be a number value \n")
+      print("Pls enter number value only")
 
 
-  print("What Percentage of tip")
   while True:
     try:
-      perc = int(input(">>"))
+      print('Whats the tip percentage')
+      perc = float(input(">> %.."))
       break
     except:
-      print("\n Must be a number Value \n")
+      print("Enter number value")
 
 
-  print("Calculating Plssss wait..>>")
+  print("Calculation in progress..hold on\n \n ")
   time.sleep(2)
 
-  bill_payment = payments(total_bill, num_ppl)
+  bill_payment = round(float(bill/people),2)
+  tip_payment = round(float(bill * (perc/100)),2)
+  bill_tip  = round(float(bill + tip_payment),2)
+  per_head = round(float(bill_tip / people),2)
 
-  tip_payment = tip(total_bill, perc, num_ppl)
 
-  total_payment = float(bill_payment) + float(tip_payment)
+  print("Each person pay without tip {}".format(bill_payment))
+  print("Total Tip pay {}".format(tip_payment))
+  print("Grand Total {}".format(bill_tip))
+  print("\n\nFinal payment needs to be pay by per head... {}".format(per_head))
 
-  print('\n\n\nEach person pays {} for the bill  '.format(str(bill_payment)))
-
-  print('Each person pays {} for the tip'.format(str(tip_payment)))
-
-  print('Each person pays total {}  '.format(str(total_payment)))
 
 
 if __name__ == '__main__':
